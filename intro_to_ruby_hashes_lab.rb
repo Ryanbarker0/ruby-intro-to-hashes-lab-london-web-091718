987ce9424e467b1ffa98d7245c60faa43fef5fd4
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-12 12:29:56 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,16 +14,16 @@ def monopoly
 			pieces: 4,
 		 	names: {
 				reading_railroad: {
-					
+					mortgage_value: "$100"
 				}
 				pennsylvania_railroad: {
-					
+
 				}
 				b_and_o_railroad: {
-					
+
 				}
 				shortline: {
-					
+
 				}
 			},
 			rent_in_dollars: {
